a0a7fb62f1b804b920fdf0236dac4e46b7f9f35c
TimScheifler <tim.scheifler@web.de>
2021-01-15 13:01:13 +0100

    Added Prometheus Added small.html + resources

--- src/main/java/com/example/restexample/PayrollApplication.java
+++ src/main/java/com/example/restexample/PayrollApplication.java
@@ -2,8 +2,6 @@ package com.example.restexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @SpringBootApplication //meta-annotation that pulls in component scanning, autoconfiguration and property support
 public class PayrollApplication {
@@ -11,11 +9,4 @@ public class PayrollApplication {
     public static void main(String... args) {
         SpringApplication.run(PayrollApplication.class, args);
     }
-
-    /*@Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("com.example.restexample"))
-                .build();
-    }*/
 }
